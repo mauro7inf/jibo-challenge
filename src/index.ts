beae0640be176be28d8d823d9d8102ef68147618
Mauro Braunstein <webmaster@offtonic.com>
2017-04-01 22:08:11 -0400

    Removed commented-out code

--- src/index.ts
+++ src/index.ts
@@ -352,36 +352,3 @@ function createSolveButton() {
   stage.addChild(text);
   solveButtonText = text;
 }
-
-/*// Declare a global variable for our sprite so that the animate function can access it.
-let bunny:PIXI.Sprite = null;
-
-// load the texture we need
-PIXI.loader.add('bunny', 'images/bunny.jpeg').load(function (loader:PIXI.loaders.Loader, resources:any) {
-    // This creates a texture from a 'bunny.png' image.
-    bunny = new PIXI.Sprite(resources.bunny.texture);
-
-    // Setup the position and scale of the bunny
-    bunny.position.x = 400;
-    bunny.position.y = 300;
-
-    bunny.scale.x = 2;
-    bunny.scale.y = 2;
-
-    // Add the bunny to the scene we are building.
-    stage.addChild(bunny);
-
-    // kick off the animation loop (defined below)
-    animate();
-});
-
-function animate() {
-    // start the timer for the next animation loop
-    requestAnimationFrame(animate);
-
-    // each frame we spin the bunny around a bit
-    bunny.rotation += 0.01;
-
-    // this is the main render call that makes pixi draw your container and its children.
-    renderer.render(stage);
-}*/
